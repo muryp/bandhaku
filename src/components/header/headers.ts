57774238a4e3381acfe7a8d24e266353f3745211
aliefprihantoro <aliefprihantoro@gmail.com>
2026-01-21 23:49:27 +0700

    feat(ui): add nav

--- src/components/header/headers.ts
+++ src/components/header/headers.ts
@@ -3,9 +3,19 @@ import './style.css'
 export const Header = html`
 <header class="main-header">
   <div class="logo">Brand<span>Logo</span></div>
-<button id="dark-mode-toggle" class="theme-toggle" aria-label="Toggle Dark Mode">
-  <img class="sun-icon" src="https://api.iconify.design/heroicons:sun-20-solid.svg?color=currentColor" alt="Sun">
-  <img class="moon-icon" src="https://api.iconify.design/heroicons:moon-20-solid.svg?color=currentColor" alt="Moon">
-</button>
+  
+  <nav class="main-nav">
+    <ul class="nav-list">
+      <li><a href="#" class="nav-link active">Home</a></li>
+      <li><a href="#" class="nav-link">Search</a></li>
+      <li><a href="#" class="nav-link">Profile</a></li>
+      <li><a href="#" class="nav-link">Settings</a></li>
+    </ul>
+  </nav>
+
+  <button id="dark-mode-toggle" class="theme-toggle">
+    <img class="sun-icon" src="https://api.iconify.design/heroicons:sun-20-solid.svg?color=currentColor">
+    <img class="moon-icon" src="https://api.iconify.design/heroicons:moon-20-solid.svg?color=currentColor">
+  </button>
 </header>
 `
